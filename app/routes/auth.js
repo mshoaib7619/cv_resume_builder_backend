2347c0e7fe477ae0bf772177f69d1dd407da1548
mshoaib7619 <shoaibkareem753@gmail.com>
2024-09-19 11:12:42 +0500

    login error

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,24 +1,24 @@
-const router = require('express').Router();
+  const router = require('express').Router();
 
-const middleware = require('../middleware');
-const controllers = require('../controllers');
-const constants = require('../constants');
+  const middleware = require('../middleware');
+  const controllers = require('../controllers/auth');
+  const constants = require('../constants');
 
-router.post('/login',
-  middleware.authenticateLogin,
-  middleware.loadUserContext,
-  controllers.auth.login,
-);
+  router.post('/login',
+    middleware.authenticateLogin,
+    middleware.loadUserContext,
+    controllers.auth.login,
+  );
 
-router.get('/logout',
-  middleware.isLoggedIn,
-  controllers.auth.logout
-);
+  router.get('/logout',
+    middleware.isLoggedIn,
+    controllers.auth.logout
+  );
 
-router.post('/signup',
-  controllers.auth.signup
-);
+  router.post('/signup',
+    controllers.auth.signup
+  );
 
 
 
-module.exports = router;
+  module.exports = router;
