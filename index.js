594db844428636a7f0d1bb9b6180bcfbc4ad49a2
mshoaib7619 <shoaibkareem753@gmail.com>
2024-09-20 19:06:55 +0500

    updated

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN || 'https://cv-resume-builder-backend-boqq.vercel.app/');
+  res.header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN || 'https://cv-resume-builder-backend.vercel.app/');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,token,authorization');
   res.header('Access-Control-Allow-Credentials', true);
